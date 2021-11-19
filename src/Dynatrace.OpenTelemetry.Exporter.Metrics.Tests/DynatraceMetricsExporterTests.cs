d00d89c45bf4f501a68ba5f125b46a66d559a3fa
Joao Grassi <joao.grassi@dynatrace.com>
2021-11-19 11:15:28 +0100

    Remove cumulative tests
    Now we don't have cumulatives anymore, so need to test. Introduced a new test to make sure if users override the reader temporality, the SDK fails fast.

--- src/Dynatrace.OpenTelemetry.Exporter.Metrics.Tests/DynatraceMetricsExporterTests.cs
+++ src/Dynatrace.OpenTelemetry.Exporter.Metrics.Tests/DynatraceMetricsExporterTests.cs
@@ -328,6 +328,42 @@ namespace Dynatrace.OpenTelemetry.Exporter.Metrics.Tests
 		}
 
 		[Fact]
+		public void Export_OverrideAggregationTemporality_ShouldThrowArgumentException()
+		{
+			// Arrange
+			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
+
+			HttpRequestMessage actualRequestMessage = null!;
+			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
+			var mockLogger = new Mock<ILogger<DynatraceMetricsExporter>>();
+			mockLogger.Setup(x => x.IsEnabled(LogLevel.Warning)).Returns(true);
+
+			var sut = new DynatraceMetricsExporter(null, mockLogger.Object, new HttpClient(mockMessageHandler.Object));
+
+			// It should not be possible to override the reader with a different temporality
+			// https://github.com/open-telemetry/opentelemetry-specification/blob/v1.8.0/specification/metrics/sdk.md#temporality-override-rules
+			var ex = Assert.Throws<ArgumentException>(() => new TestMetricReader(sut)
+			{
+				PreferredAggregationTemporality = AggregationTemporality.Cumulative
+			});
+
+			Assert.Equal(
+				"PreferredAggregationTemporality Cumulative and SupportedAggregationTemporality Delta are incompatible.",
+				ex.Message);
+
+			// It should not be possible to override the reader with a different temporality
+			// https://github.com/open-telemetry/opentelemetry-specification/blob/v1.8.0/specification/metrics/sdk.md#temporality-override-rules
+			ex = Assert.Throws<ArgumentException>(() => new TestMetricReader(sut)
+			{
+				SupportedAggregationTemporality = AggregationTemporality.Cumulative
+			});
+
+			Assert.Equal(
+				"PreferredAggregationTemporality Delta and SupportedAggregationTemporality Cumulative are incompatible.",
+				ex.Message);
+		}
+
+		[Fact]
 		public async Task Export_SimulateMultipleExports_ShouldExportCorrectDelta()
 		{
 			// Arrange
@@ -417,61 +453,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_LongSumCumulative_ShouldExportAsGauge()
-		{
-			// Arrange
-			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
-
-			HttpRequestMessage actualRequestMessage = null!;
-			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
-			var mockLogger = new Mock<ILogger<DynatraceMetricsExporter>>();
-			mockLogger.Setup(x => x.IsEnabled(LogLevel.Warning)).Returns(true);
-
-			var sut = new DynatraceMetricsExporter(null, mockLogger.Object, new HttpClient(mockMessageHandler.Object));
-
-			var metricReader = new TestMetricReader(sut)
-			{
-				PreferredAggregationTemporality = AggregationTemporality.Cumulative
-			};
-			using var provider = Sdk.CreateMeterProviderBuilder()
-				.AddMeter(meter.Name)
-				.AddReader(metricReader)
-				.Build();
-
-			var counter = meter.CreateCounter<int>("counter");
-			counter.Add(10, _attributes);
-
-			// Act - Reader will call our exporter
-			metricReader.Collect();
-
-			// Assert
-			var exportedMetrics = metricReader.GetExportedMetrics();
-			var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
-
-			var expected = $"counter,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,10 {point.TimeStamp}";
-			var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
-			Assert.Equal(expected, actualMetricString);
-
-			mockMessageHandler.Protected().Verify(
-				"SendAsync",
-				Times.Exactly(1),
-				ItExpr.IsAny<HttpRequestMessage>(),
-				ItExpr.IsAny<CancellationToken>());
-
-			AssertExportRequest(actualRequestMessage);
-
-			mockLogger.Verify(x => x.Log(
-				LogLevel.Warning,
-				7, // eventid define in LoggerExtensions
-				It.Is<It.IsAnyType>((value, type) =>
-					value.ToString()!.Contains("Received metric: 'counter' with cumulative aggregation temporality. Exporting as gauge")),
-				It.IsAny<Exception>(),
-				It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-				Times.Exactly(1));
-		}
-
-		[Fact]
-		public async Task Export_LongSumDelta()
+		public async Task Export_LongSum_ShouldExportAsDelta()
 		{
 			// Arrange
 			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
@@ -481,7 +463,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
+			// Delta is already the preferred/supported temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -512,61 +494,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_DoubleSumCumulative_ShouldExportAsGauge()
-		{
-			// Arrange
-			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
-
-			HttpRequestMessage actualRequestMessage = null!;
-			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
-			var mockLogger = new Mock<ILogger<DynatraceMetricsExporter>>();
-			mockLogger.Setup(x => x.IsEnabled(LogLevel.Warning)).Returns(true);
-
-			var sut = new DynatraceMetricsExporter(null, mockLogger.Object, new HttpClient(mockMessageHandler.Object));
-
-			var metricReader = new TestMetricReader(sut)
-			{
-				PreferredAggregationTemporality = AggregationTemporality.Cumulative
-			};
-			using var provider = Sdk.CreateMeterProviderBuilder()
-				.AddMeter(meter.Name)
-				.AddReader(metricReader)
-				.Build();
-
-			var counter = meter.CreateCounter<double>("double_counter");
-			counter.Add(10.3, _attributes);
-
-			// Act - Reader will call our exporter
-			metricReader.Collect();
-
-			// Assert
-			var exportedMetrics = metricReader.GetExportedMetrics();
-			var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
-
-			var expected = $"double_counter,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,10.3 {point.TimeStamp}";
-			var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
-			Assert.Equal(expected, actualMetricString);
-
-			mockMessageHandler.Protected().Verify(
-				"SendAsync",
-				Times.Exactly(1),
-				ItExpr.IsAny<HttpRequestMessage>(),
-				ItExpr.IsAny<CancellationToken>());
-
-			AssertExportRequest(actualRequestMessage);
-
-			mockLogger.Verify(x => x.Log(
-				LogLevel.Warning,
-				7, // eventid define in LoggerExtensions
-				It.Is<It.IsAnyType>((value, type) =>
-					value.ToString()!.Contains("Received metric: 'double_counter' with cumulative aggregation temporality. Exporting as gauge")),
-				It.IsAny<Exception>(),
-				It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-				Times.Exactly(1));
-		}
-
-		[Fact]
-		public async Task Export_DoubleSumDelta()
+		public async Task Export_DoubleSum_ShouldExportAsDelta()
 		{
 			// Arrange
 			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
@@ -576,7 +504,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
+			// Delta is already the preferred/supported temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -607,7 +535,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_ObservableLongCounterDelta()
+		public async Task Export_ObservableLongCounter_ShouldExportAsDelta()
 		{
 			// Arrange
 			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
@@ -617,7 +545,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
+			// Delta is already the preferred/supported temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -636,7 +564,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 			// Perform two exports to ensure deltas are exported correctly
 			metricReader.Collect();
 			await AssertLines(metricReader, 10);
-			
+
 			metricReader.Collect();
 			await AssertLines(metricReader, 10);
 
@@ -660,7 +588,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_ObservableLongCounterCumulative_ShouldExportAsGauge()
+		public async Task Export_ObservableDoubleCounter_ShouldExportAsDelta()
 		{
 			// Arrange
 			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
@@ -670,62 +598,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			var metricReader = new TestMetricReader(sut)
-			{
-				PreferredAggregationTemporality = AggregationTemporality.Cumulative
-			};
-			using var provider = Sdk.CreateMeterProviderBuilder()
-				.AddMeter(meter.Name)
-				.AddReader(metricReader)
-				.Build();
-
-			var i = 1;
-			var observableCounter = meter.CreateObservableCounter("obs_counter", () =>
-			{
-				return new List<Measurement<long>>()
-				{
-					new Measurement<long>(i++ * 10, _attributes)
-				};
-			});
-
-			// Perform two exports to ensure cumulatives are exported as gauges correctly
-			metricReader.Collect();
-			await AssertLines(metricReader, 10);
-
-			metricReader.Collect();
-			await AssertLines(metricReader, 20);
-
-			async Task AssertLines(TestMetricReader metricReader, long expectedValue)
-			{
-				var exportedMetrics = metricReader.GetExportedMetrics();
-				var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
-
-				var expected = $"obs_counter,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,{expectedValue} {point.TimeStamp}";
-				var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
-				Assert.Equal(expected, actualMetricString);
-			}
-
-			mockMessageHandler.Protected().Verify(
-				"SendAsync",
-				Times.Exactly(2),
-				ItExpr.IsAny<HttpRequestMessage>(),
-				ItExpr.IsAny<CancellationToken>());
-
-			AssertExportRequest(actualRequestMessage);
-		}
-
-		[Fact]
-		public async Task Export_ObservableDoubleCounterDelta()
-		{
-			// Arrange
-			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
-
-			HttpRequestMessage actualRequestMessage = null!;
-			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
-
-			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
-
-			// Delta is already the preferred temporality for our exporter
+			// Delta is already the preferred/temporality temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -768,61 +641,6 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_ObservableDoubleCounterCumulative_ShouldExportAsGauge()
-		{
-			// Arrange
-			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
-
-			HttpRequestMessage actualRequestMessage = null!;
-			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
-
-			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
-
-			var metricReader = new TestMetricReader(sut)
-			{
-				PreferredAggregationTemporality = AggregationTemporality.Cumulative
-			};
-			using var provider = Sdk.CreateMeterProviderBuilder()
-				.AddMeter(meter.Name)
-				.AddReader(metricReader)
-				.Build();
-
-			var i = 1;
-			var observableCounter = meter.CreateObservableCounter("double_obs_counter", () =>
-			{
-				return new List<Measurement<double>>()
-				{
-					new(i++ * 10.3, _attributes)
-				};
-			});
-
-			// Perform two exports to ensure cumulatives are exported as gauges correctly
-			metricReader.Collect();
-			await AssertLines(metricReader, 10.3);
-
-			metricReader.Collect();
-			await AssertLines(metricReader, 20.6);
-
-			async Task AssertLines(TestMetricReader metricReader, double expectedValue)
-			{
-				var exportedMetrics = metricReader.GetExportedMetrics();
-				var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
-
-				var expected = $"double_obs_counter,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,{expectedValue} {point.TimeStamp}";
-				var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
-				Assert.Equal(expected, actualMetricString);
-			}
-
-			mockMessageHandler.Protected().Verify(
-				"SendAsync",
-				Times.Exactly(2),
-				ItExpr.IsAny<HttpRequestMessage>(),
-				ItExpr.IsAny<CancellationToken>());
-
-			AssertExportRequest(actualRequestMessage);
-		}
-
-		[Fact]
 		public async Task Export_ObservableLongGauge()
 		{
 			// Arrange
@@ -833,18 +651,17 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
 				.AddReader(metricReader)
 				.Build();
 
-			var observableCounter = meter.CreateObservableGauge<long>("gauge", () =>
+			var observableCounter = meter.CreateObservableGauge("gauge", () =>
 			{
 				return new List<Measurement<long>>()
 				{
-					new Measurement<long>(10, _attributes)
+					new (10, _attributes)
 				};
 			});
 
@@ -879,18 +696,17 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
 				.AddReader(metricReader)
 				.Build();
 
-			var observableCounter = meter.CreateObservableGauge<double>("double_gauge", () =>
+			var observableCounter = meter.CreateObservableGauge("double_gauge", () =>
 			{
 				return new List<Measurement<double>>()
 				{
-					new Measurement<double>(10.3, _attributes),
+					new (10.3, _attributes),
 				};
 			});
 
@@ -925,7 +741,6 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -971,7 +786,6 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
@@ -1009,60 +823,6 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 		}
 
 		[Fact]
-		public async Task Export_View_CounterWithCumulativeTemporality()
-		{
-			// Arrange
-			using var meter = new Meter(TestUtils.GetCurrentMethodName(), "0.0.1");
-
-			HttpRequestMessage actualRequestMessage = null!;
-			var mockMessageHandler = SetupHttpMock((HttpRequestMessage r) => actualRequestMessage = r);
-
-			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
-
-			var metricReader = new TestMetricReader(sut)
-			{
-				PreferredAggregationTemporality = AggregationTemporality.Cumulative
-			};
-			using var provider = Sdk.CreateMeterProviderBuilder()
-				.AddMeter(meter.Name)
-				.AddReader(metricReader)
-				.AddView(instrumentName: "counter", name: "myview")
-				.Build();
-
-			var counter = meter.CreateCounter<long>("counter");
-
-			counter.Add(10, _attributes);
-			metricReader.Collect();
-			await AssertLines(metricReader, 10);
-
-			counter.Add(20, _attributes);
-			metricReader.Collect();
-			await AssertLines(metricReader, 30);
-
-			counter.Add(30, _attributes);
-			metricReader.Collect();
-			await AssertLines(metricReader, 60);
-
-			async Task AssertLines(TestMetricReader metricReader, long expectedValue)
-			{
-				var exportedMetrics = metricReader.GetExportedMetrics();
-				var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
-
-				var expected = $"myview,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,{expectedValue} {point.TimeStamp}";
-				var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
-				Assert.Equal(expected, actualMetricString);
-			}
-
-			AssertExportRequest(actualRequestMessage);
-
-			mockMessageHandler.Protected().Verify(
-				"SendAsync",
-				Times.Exactly(3),
-				ItExpr.IsAny<HttpRequestMessage>(),
-				ItExpr.IsAny<CancellationToken>());
-		}
-
-		[Fact]
 		public async Task Export_View_CounterWithDeltaTemporality()
 		{
 			// Arrange
@@ -1073,7 +833,6 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var sut = new DynatraceMetricsExporter(null, null, new HttpClient(mockMessageHandler.Object));
 
-			// Delta is already the preferred temporality for our exporter
 			var metricReader = new TestMetricReader(sut);
 			using var provider = Sdk.CreateMeterProviderBuilder()
 				.AddMeter(meter.Name)
