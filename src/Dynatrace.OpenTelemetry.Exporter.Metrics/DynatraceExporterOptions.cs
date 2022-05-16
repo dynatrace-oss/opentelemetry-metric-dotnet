ed3a0a056078f7916728a092209329fb9b44fa65
marc.pichler <marc.pichler@dynatrace.com>
2022-05-16 13:19:53 +0200

    remove http client from options.

--- src/Dynatrace.OpenTelemetry.Exporter.Metrics/DynatraceExporterOptions.cs
+++ src/Dynatrace.OpenTelemetry.Exporter.Metrics/DynatraceExporterOptions.cs
@@ -15,7 +15,6 @@
 // </copyright>
 
 using System.Collections.Generic;
-using System.Net.Http;
 
 namespace Dynatrace.OpenTelemetry.Exporter.Metrics
 {
@@ -63,10 +62,5 @@ namespace Dynatrace.OpenTelemetry.Exporter.Metrics
 		/// Gets or sets the metric export interval in milliseconds. The default value is 1 minute (60000 ms).
 		/// </summary>
 		public int MetricExportIntervalMilliseconds { get; set; } = 60000;
-
-		/// <summary>
-		/// Gets or sets the <see cref="HttpClient"/> that is used by the exporter
-		/// </summary>
-		internal HttpClient Client { get; set; } = null;
 	}
 }
