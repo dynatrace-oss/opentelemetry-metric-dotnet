46bef3b100411a16a4e89583a5ae3d2bd2944448
marc.pichler <marc.pichler@dynatrace.com>
2022-06-02 14:17:14 +0200

    Use MemberData instead of InlineData.

--- src/Dynatrace.OpenTelemetry.Exporter.Metrics.Tests/DynatraceMetricsExporterTests.cs
+++ src/Dynatrace.OpenTelemetry.Exporter.Metrics.Tests/DynatraceMetricsExporterTests.cs
@@ -33,6 +33,16 @@ using Xunit;
 
 namespace Dynatrace.OpenTelemetry.Exporter.Metrics.Tests
 {
+	public sealed class HistogramTestData
+	{
+		public double[] Boundaries { get; init; } = Array.Empty<double>();
+		public int Count { get; init; }
+		public double[] Values { get; init; } = Array.Empty<double>();
+		public double Sum { get; init; }
+		public double Min { get; init; }
+		public double Max { get; init; }
+	}
+
 	public sealed class DynatraceMetricsExporterTests : IDisposable
 	{
 		private readonly TagList _attributes = new() { { "attr1", "v1" }, { "attr2", "v2" } };
@@ -662,34 +672,184 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 			AssertExportRequest(actualRequestMessage);
 		}
 
+		public static IEnumerable<object[]> TestData =>
+			new List<object[]>
+			{
+				new object[]
+				{
+					// min: Values between the first two boundaries.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 6,
+						Values = new[] { 1.5, 3.2, 3.2, 3.5, 4.8, 4.9 },
+						Sum = 21.1,
+						Min = 1d,
+						Max = 5d
+					}
+				},
+				new object[]
+				{
+					// min: First bucket has value, use the first boundary as estimation instead of Inf.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 8,
+						Values = new[] { 0.5, 3.1, 3.1, 3.1, 6, 6, 6, 6.7 },
+						Sum = 34.5,
+						Min = 1d,
+						Max = 5d
+					}
+				},
+				new object[]
+				{
+					// min: Only the first bucket has values, use the mean (0.25) Otherwise, the min would be estimated as 1, and min <= avg would be violated.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 3,
+						Values = new[] { 0.3, 0.3, 0.15 },
+						Sum = 0.75,
+						Min = 0.25,
+						Max = 1
+					},
+				},
+				new object[]
+				{
+					// min: Just one bucket from -Inf to Inf, calculate the mean as min value.
+					new HistogramTestData
+					{
+						Boundaries = Array.Empty<double>(),
+						Count = 4,
+						Values = new[] { 2, 2, 2, 2.8 },
+						Sum = 8.8,
+						Min = 2.2,
+						Max = 2.2
+					}
+				},
+				new object[]
+				{
+					// min: Just one bucket from -Inf to Inf, calculate the mean as min value.
+					new HistogramTestData
+					{
+						Boundaries = Array.Empty<double>(),
+						Count = 1,
+						Values = new[] { 1.2 },
+						Sum = 1.2,
+						Min = 1.2,
+						Max = 1.2
+					},
+				},
+				new object[]
+				{
+					// min: Only the last bucket has a value, use the lower bound.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 3,
+						Values = new[] { 5.1, 5.1, 5.4 },
+						Sum = 15.6,
+						Min = 5,
+						Max = 5.2
+					}
+				},
+				new object[]
+				{
+					// max: Values between the last two boundaries.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 6,
+						Values = new[] { 1.5, 3.2, 3.2, 3.5, 4.8, 4.9 },
+						Sum = 21.1,
+						Min = 1d,
+						Max = 5d
+					}
+				},
+				new object[]
+				{
+					// max: Values between the last two boundaries.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 8,
+						Values = new[] { 0.5, 3.1, 3.1, 3.1, 6, 6, 6, 6.7 },
+						Sum = 34.5,
+						Min = 1,
+						Max = 5
+					}
+				},
+				new object[]
+				{
+					// max: Only the last bucket has values, use the mean (10.1) Otherwise, the max would be estimated as 5, and max >= avg would be violated.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 2,
+						Values = new[] { 5.2, 15 },
+						Sum = 20.2,
+						Min = 5,
+						Max = 10.1
+					}
+				},
+				new object[]
+				{
+					// max: Just one bucket from -Inf to Inf, calculate the mean as max value.
+					new HistogramTestData
+					{
+						Boundaries = Array.Empty<double>(),
+						Count = 4,
+						Values = new[] { 2, 2, 2, 2.8 },
+						Sum = 8.8,
+						Min = 2.2,
+						Max = 2.2
+					}
+				},
+				new object[]
+				{
+					// max: Just one bucket from -Inf to Inf, calculate the mean as max value.
+					new HistogramTestData
+					{
+						Boundaries = Array.Empty<double>(),
+						Count = 1,
+						Values = new[] { 1.2 },
+						Sum = 1.2,
+						Min = 1.2,
+						Max = 1.2
+					}
+				},
+				new object[]
+				{
+					// max: Max is larger than the sum, use the estimated boundary.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 0d, 5d },
+						Count = 2,
+						Values = new[] { 1, 1.3 },
+						Sum = 2.3,
+						Min = 0,
+						Max = 5d
+					}
+				},
+				new object[]
+				{
+					// max: Only the first bucket has a value, use the upper bound.
+					new HistogramTestData
+					{
+						Boundaries = new[] { 1d, 2d, 3d, 4d, 5d },
+						Count = 3,
+						Values = new[] { 0.2, 0.4, 0.9 },
+						Sum = 1.5,
+						Min = 0.5,
+						Max = 1
+					}
+				}
+			};
+
+
 		[Theory]
-		// min: Values between the first two boundaries.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 6, new[] { 1.5, 3.2, 3.2, 3.5, 4.8, 4.9 },  21.1, 1d, 5d)]
-		// min: First bucket has value, use the first boundary as estimation instead of Inf.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 8, new[] { 0.5, 3.1, 3.1, 3.1, 6, 6, 6, 6.7 }, 34.5, 1d, 5d)]
-		// min: Only the first bucket has values, use the mean (0.25) Otherwise, the min would be estimated as 1, and min <= avg would be violated.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 3, new[] { 0.3, 0.3, 0.15 }, 0.75, 0.25, 1)]
-		// min: Just one bucket from -Inf to Inf, calculate the mean as min value.
-		[InlineData(new double[0], 4, new[] { 2, 2, 2, 2.8 }, 8.8, 2.2, 2.2)]
-		// min: Just one bucket from -Inf to Inf, calculate the mean as min value.
-		[InlineData(new double[0], 1, new[] { 1.2 }, 1.2, 1.2, 1.2)]
-		// min: Only the last bucket has a value, use the lower bound.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 3, new[] { 5.1, 5.1, 5.4 }, 15.6, 5, 5.2)]
-		// max: Values between the last two boundaries.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 6, new[] { 1.5, 3.2, 3.2, 3.5, 4.8, 4.9 }, 21.1, 1d, 5d)]
-		// max: Values between the last two boundaries.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 8, new[] { 0.5, 3.1, 3.1, 3.1, 6, 6, 6, 6.7 }, 34.5, 1, 5)]
-		// max: Only the last bucket has values, use the mean (10.1) Otherwise, the max would be estimated as 5, and max >= avg would be violated.
-		[InlineData(new[] { 1d, 2d, 3d, 4d, 5d }, 2, new[] { 5.2, 15 }, 20.2, 5, 10.1)]
-		// max: Just one bucket from -Inf to Inf, calculate the mean as max value.
-		[InlineData(new double[0], 4, new[] { 2, 2, 2, 2.8 }, 8.8, 2.2, 2.2)]
-		// max: Just one bucket from -Inf to Inf, calculate the mean as max value.
-		[InlineData(new double[0], 1, new[] { 1.2 }, 1.2, 1.2, 1.2)]
-		// max: Max is larger than the sum, use the estimated boundary.
-		[InlineData(new[] { 0d, 5d }, 2, new[] { 1, 1.3 }, 2.3, 0, 5d)]
-		// max: Only the first bucket has a value, use the upper bound.
-		[InlineData(new[] {1d, 2d, 3d, 4d, 5d}, 3, new [] {0.2, 0.4, 0.9}, 1.5, 0.5, 1)]
-		public async Task Export_Histogram_ShouldSetMinAndMaxCorrectly(double[] boundaries, int count, double[] values, double sum, double min, double max)
+		[MemberData(nameof(TestData))]
+		public async Task Export_Histogram_ShouldSetMinAndMaxCorrectly(HistogramTestData testData)
 		{
 			// Arrange
 			using var meter = new Meter(Guid.NewGuid().ToString(), "0.0.1");
@@ -701,7 +861,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 					options => options.TemporalityPreference = MetricReaderTemporalityPreference.Delta)
 				.AddView(
 					instrumentName: "histogram",
-					new ExplicitBucketHistogramConfiguration { Boundaries = boundaries })
+					new ExplicitBucketHistogramConfiguration { Boundaries = testData.Boundaries })
 				.Build();
 
 			HttpRequestMessage actualRequestMessage = null!;
@@ -711,7 +871,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 
 			var histogram = meter.CreateHistogram<double>("histogram");
 
-			foreach (var value in values)
+			foreach (var value in testData.Values)
 			{
 				histogram.Record(value, _attributes);
 			}
@@ -723,7 +883,7 @@ counterB,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry count,delta=20 {point
 			// Assert
 			var point = MetricTest.FromMetricPoints(exportedMetrics.First().GetMetricPoints()).First();
 
-			var expected = $"histogram,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,min={min},max={max},sum={sum},count={count} {point.TimeStamp}";
+			var expected = $"histogram,attr1=v1,attr2=v2,dt.metrics.source=opentelemetry gauge,min={testData.Min},max={testData.Max},sum={testData.Sum},count={testData.Count} {point.TimeStamp}";
 			var actualMetricString = await actualRequestMessage.Content!.ReadAsStringAsync();
 			Assert.Equal(expected, actualMetricString);
 
